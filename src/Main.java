240713793b3227f0201f481b627522dacd4032ca
Neenu <neenubabychan@gmail.com>
2022-10-20 11:28:52 +0530

    alphabet4 master update

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,7 @@ public class Main {
         System.out.println("Text 2");
         System.out.println("Change from project 4");
         System.out.println("Change from develop branch-alphabet1");
-        System.out.println("Text new change1");
-        System.out.println("Text new change2");
+        
 
 
     }
